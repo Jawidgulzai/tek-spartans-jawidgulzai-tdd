5f88dc3ff0f7bd7b62b941cfa54f122c341d5391
Jawid <jawidgulzai9@gmail.com>
2024-08-13 16:28:52 -0700

    TestNG dependencies added

--- src/test/java/playground/Activity1.java
+++ src/test/java/playground/Activity1.java
@@ -1,13 +1,12 @@
 package playground;
-
-import org.junit.Assert;
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-public class ActivityOne {
+public class Activity1 {
 
     /*
     Write a test that navigates to Facebook.com
